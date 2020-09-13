232eb16a22292ccd5d09c8577ae5ea83607e1c3e
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2020-09-13 21:13:25 +0200

    added some more provisioning steps for graphics and interactive ssh support.

--- Vagrantfile
+++ Vagrantfile
@@ -7,6 +7,8 @@ Vagrant.configure("2") do |config|
   config.vm.box = "hashicorp/bionic64"
   config.vm.provision :shell, path: "bootstrap.sh"
 
+  config.vm.synced_folder ".", "/vagrant"
+
   config.vm.provider "virtualbox" do |vb|
   #   vb.gui = true
     vb.memory = "2048"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+# Install graphics drivers (pyqt5 compatibility)
+sudo apt update
+sudo apt install libgl1-mesa-glx
+
+
 # Install Conda
 wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash ./miniconda.sh -b
 export PATH=~/miniconda3/bin:$PATH
+echo "export PATH=~/miniconda3/bin:$PATH" >> ~/.bashrc
+
 conda update --yes conda
 
 # Go to project directory
